a3a90b267988ee5ed7d61b064958dbb002ee1c0a
Ryan JT <ryantalbot7@gmail.com>
2021-01-10 17:17:26 -0600

    les. 6

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion';
 
 class QuizQuestionButton extends Component {
 	render() {
 		return (
-			<li>
-				<button>{this.props.button_text}</button>
-			</li>
+			<ul this.props.quiz_question.map((answer_options, index) => {
+				return <button key={index}>{button_text.answer_options
+				}</button>
+			}) >
+
+				<li >
+					<button>{this.props.button_text}</button>
+				</li>
+
+			</ul>
+
 		);
 	}
 }
